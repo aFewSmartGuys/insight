993324b863e0d96f869ab30d3cf8633c0f992ffc
Santiago Verdu <santiago@cl.ly>
2020-08-13 22:25:54 -0600

    name request

--- src/main.go
+++ src/main.go
@@ -2,31 +2,47 @@ package main
 
 import (
   "fmt"
+  "net/http"
 )
 
 type Plugin interface {
   Run() bool
+  Name() string
 }
 
 type Http struct {
   domain string
-  method string
-  port int
+  name string
 }
 
 func (h Http) Run() bool {
-  fmt.Println("running http plugin.")
-  return true
+  fmt.Println("Running http plugin.")
+  resp, err := http.Get(h.domain)
+
+  if err != nil {
+    return false
+  }
+
+  return resp.StatusCode == 200
 }
 
-func runPlugins(p [1]Plugin) {
+func (h Http) Name() string {
+  return h.name
+}
+
+func runPlugins(p []Plugin) {
   for i := 0; i < len(p); i++ {
-    p[i].Run()
+    success := p[i].Run()
+    if success {
+      fmt.Println(p[i].Name(), "is up.")
+    } else {
+      fmt.Println(p[i].Name(), "is down.")
+    }
   }
 }
 
 func main() {
-  plugins := [1]Plugin{Http{"archlinux.org", "GET", 80}}
+  plugins := []Plugin{Http{"https://archlinux.org", "Arch Linux"}}
   runPlugins(plugins)
 }
 
